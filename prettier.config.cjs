7c478af2fbbb92f0896d9ecc24c6c5f2c3eec2af
jainChetanUnolo <chetan@unolo.com>
2023-02-03 12:23:15 +0530

    first commit

---
+++ prettier.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+	printWidth: 140,
+	endOfLine: "auto",
+	trailingComma: "none",
+	semi: true,
+	doubleQuote: true,
+	jsxSingleQuote: false,
+	bracketSameLine: true,
+	jsxDoubleQuote: true,
+	singleQuote: false,
+	useTabs: true,
+	tabWidth: 2
+};
---
+++ src/component/useFetch.tsx
@@ -0,0 +1,52 @@
+import { useEffect, useState, useDebugValue, useRef, useCallback } from "react";
+export default function useFetch<TFetch_Type>(url: string, method: "GET" | "POST" = "GET", body: BodyInit | undefined = undefined) {
+	const [numberOfRefetch, setNumberOfRefetch] = useState(0);
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState<unknown | null>(null);
+	const [data, setData] = useState<TFetch_Type | null>(null);
+	const controllerRef = useRef<AbortController | null>(null);
+	useDebugValue([data, loading, error]);
+	const refetch = async () => {
+		setNumberOfRefetch((prev) => prev + 1);
+		await fetchData(false);
+	};
+
+	const fetchData = useCallback(
+		async (isCancelled: boolean) => {
+			if (controllerRef.current) controllerRef.current.abort();
+			const controller = new AbortController();
+			controllerRef.current = controller;
+			if (!url || isCancelled) return;
+			setLoading(true);
+			try {
+				const response = await fetch(`${url}`, {
+					signal: controllerRef.current?.signal,
+					credentials: "include",
+					method,
+					body
+				});
+				const newData = (await response.json()) as TFetch_Type;
+				if (!isCancelled && response.ok) {
+					setData(newData);
+					setError(null);
+					controllerRef.current = null;
+				}
+			} catch (e: unknown) {
+				setError(e);
+			}
+			setLoading(false);
+		},
+		[body, method, url]
+	);
+
+	useEffect(() => {
+		let isCancelled = false;
+		if (url) fetchData(isCancelled);
+		return () => {
+			isCancelled = true;
+			controllerRef.current?.abort();
+		};
+	}, [body, method, url, fetchData]);
+
+	return { data, loading, error, refetch, refetchCount: numberOfRefetch };
+}
